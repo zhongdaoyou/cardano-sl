1072e40e09d415c5c1e8903577d3eeb1edd2a61c
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-01-18 20:51:30 +0100

    [CSL-1570] Start with Undo and UserSecret instances for Arbitrary

--- block/src/Pos/Block/Types.hs
+++ block/src/Pos/Block/Types.hs
@@ -27,6 +27,7 @@ import qualified Data.Text.Buildable
 import           Ether.Internal (HasLens (..))
 import           Formatting (bprint, build, (%))
 import           Serokell.Util.Text (listJson)
+import           Test.QuickCheck (Arbitrary (..))
 
 import           Pos.Block.Slog.Types (SlogUndo (..))
 import           Pos.Communication.Protocol (NodeId)
@@ -46,6 +47,14 @@ data Undo = Undo
     } deriving (Generic)
 
 instance NFData Undo
+
+instance Arbitrary Undo where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        c <- arbitrary
+        d <- arbitrary
+        return $ Undo a b c d
 
 -- | Block and its Undo.
 type Blund = (Block, Undo)
